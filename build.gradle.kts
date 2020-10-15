e6cf69577082c557d817cdd1c79f4eef4b59efb6
a150238 <eivind.askestad@nav.no>
2020-10-15 15:10:51 +0200

    Release setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.tasks.*
 
 plugins {
     kotlin("jvm") version "1.4.10"
+    id("se.patrikerdes.use-latest-versions") version "0.2.14"
+    id("net.researchgate.release") version "2.8.1"
+    `maven-publish`
+    `java-library`
 }
 
-group = "no.nav.security"
+group = "no.nav.pensjonsamhandling"
 version = "0.1"
 
 repositories {
@@ -18,6 +22,24 @@ dependencies {
     implementation("org.junit.jupiter", "junit-jupiter", "5.7.0")
 //    testImplementation(kotlin("test-junit5"))
     testImplementation("com.github.tomakehurst", "wiremock", "2.27.2")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/navikt/${rootProject.name}")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 tasks {
